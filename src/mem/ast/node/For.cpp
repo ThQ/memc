6d2dde87aebe94cdf9115fd76435b4d5182d42c9
Thomas Quémard <thomas.quemard@gmail.com>
2012-07-29 18:04:30 +0200

    Improve auto validation for nodes: * For * While

--- src/mem/ast/node/For.cpp
+++ src/mem/ast/node/For.cpp
@@ -14,7 +14,13 @@ For::isValid (NodeValidator* v)
 {
    Node::isValid(v);
 
-   // FIXME Must do some checks here
+   if (ConditionNode() != NULL)
+   {
+      v->ensure(ConditionNode()->ExprType()->Name() == "bool",
+         "For: condition node must have a bool expression type");
+   }
+
+   v->ensure(BlockNode() != NULL, "For node must have a block node");
 }
 
 } } }
--- src/mem/ast/node/While.cpp
+++ src/mem/ast/node/While.cpp
@@ -14,6 +14,14 @@ While::isValid (NodeValidator* v)
 {
    Node::isValid(v);
    v->ensure(ChildCount() == 2, "While must have exactly 2 children");
+
+   if (ConditionNode() != NULL)
+   {
+      v->ensure(ConditionNode()->ExprType()->Name() == "bool",
+         "While: condition node must have a bool expression type");
+   }
+
+   v->ensure(BodyNode() != NULL, "While node must have a block node");
 }
 
 
