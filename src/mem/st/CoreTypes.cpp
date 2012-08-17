8ef378ec54551bdb73eaa60225574e50a00b9118
Thomas Quémard <thomas.quemard@gmail.com>
2012-08-18 01:47:57 +0200

    Add CoreTypes.cpp

---
+++ src/mem/st/CoreTypes.cpp
@@ -0,0 +1,33 @@
+#include "mem/st/CoreTypes.hpp"
+
+namespace mem { namespace st {
+
+CoreTypes::CoreTypes ()
+{
+   _void_ty = new st::VoidType();
+
+   // Integer types
+   _bool_ty = new st::IntType("bool", sizeof(bool));
+
+   _short_ty = new st::IntType("short", sizeof(short));
+   _char_ty = new st::IntType("char", sizeof(short));
+   _int_ty = new st::IntType("int", sizeof(short));
+   _long_ty = new st::IntType("long", sizeof(short));
+
+   _ushort_ty = new st::IntType("ushort", sizeof(unsigned short));
+   _uchar_ty = new st::IntType("uchar", sizeof(unsigned short));
+   _uint_ty = new st::IntType("uint", sizeof(unsigned short));
+   _ulong_ty = new st::IntType("ulong", sizeof(unsigned short));
+
+   _int8_ty = new st::IntType("int8", 1);
+   _int16_ty = new st::IntType("int16", 2);
+   _int32_ty = new st::IntType("int32", 4);
+   _int64_ty = new st::IntType("int64", 8);
+
+   _uint8_ty = new st::IntType("uint8", 1);
+   _uint16_ty = new st::IntType("uint16", 2);
+   _uint32_ty = new st::IntType("uint32", 4);
+   _uint64_ty = new st::IntType("uint64", 8);
+}
+
+} }
