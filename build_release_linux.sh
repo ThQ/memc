fbff8c2d46a2c22696321a49de72b87787f43bdb
Thomas Quémard <thomas.quemard@gmail.com>
2012-10-20 01:34:46 +0200

    Add script to make a release build

---
+++ build_release_linux.sh
@@ -0,0 +1,13 @@
+export CCC_ANALYZER_CPLUSPLUS="true"
+export CCC_CXX=`which clang++`
+
+cwd=`pwd`
+if [ ! -d "${cwd}/build" ] ; then mkdir "${cwd}/build" ; fi
+
+cd "${cwd}/build"
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_DEBUG="-Wall -O2" \
+   && make \
+   && make test
+
+cd "${cwd}"
