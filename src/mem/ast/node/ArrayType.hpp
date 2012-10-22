496f7e684728f6d9aa79031033c2b9a810b52e52
Thomas Quémard <thomas.quemard@gmail.com>
2012-10-22 22:47:00 +0200

    Make bb test `function.return.ArrayType` pass

--- src/mem/ast/node/ArrayType.hpp
+++ src/mem/ast/node/ArrayType.hpp
@@ -14,7 +14,7 @@ namespace mem { namespace ast { namespace node {
 // Ex:
 //    [int]
 //    [int, 10]
-class ArrayType : public Id
+class ArrayType : public Type
 {
    public:
    static const int kTYPE = MetaKind::ARRAY;
